a1e93bfab7459125629eed3b380eddd46c50b232
Rodrigo Castro <rscastro@br.ibm.com>
2017-04-06 10:19:09 -0300

    ajustando tela de login

--- Wishlist/app/src/main/java/com/castrodev/wishlist/MainActivity.java
+++ Wishlist/app/src/main/java/com/castrodev/wishlist/MainActivity.java
@@ -3,8 +3,16 @@ package com.castrodev.wishlist;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -13,6 +21,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
+    private static final String TAG = MainActivity.class.getName();
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -31,21 +40,46 @@ public class MainActivity extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                 } else {
-                    // User is signed out
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setLogo(R.mipmap.ic_launcher_round)
-                                    .setIsSmartLockEnabled(false)
-                                    .setProviders(
-                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                                    .build(),
-                            RC_SIGN_IN);
+//                     User is signed out
+//                    login();
                 }
             }
         };
+
+
+        mFirebaseAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
+
+    private void login() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setLogo(R.mipmap.ic_launcher_round)
+                        .setIsSmartLockEnabled(false)
+                        .setProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(),
+                RC_SIGN_IN);
+    }
+
 
     @Override
     protected void onResume() {
@@ -58,6 +92,27 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         if (mAuthStateListener != null) {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            case R.id.login:
+                login();
+            default:
+                return super.onOptionsItemSelected(item);
+
         }
     }
 }
